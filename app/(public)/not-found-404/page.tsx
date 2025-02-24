a429c33abaa6c1147ac2904ac34ce3a46201e252
Ariel Spencer <arielspencer.tech@gmail.com>
2025-02-23 22:55:27 -0300

    style: update 404 page colors and font sizes

--- app/(public)/not-found-404/page.tsx
+++ app/(public)/not-found-404/page.tsx
@@ -4,9 +4,9 @@ import { TbError404 } from "react-icons/tb";
 export default function NotFound404() {
   return (
     <main className="flex items-center justify-center h-screen max-h-[calc(100vh-4rem)]">
-      <div className="text-center py-12 px-20 bg-b-section-2 text-t-dark-main rounded-lg shadow-lg">
+      <div className="text-center py-12 px-20 bg-btn-text rounded-lg shadow-lg">
         <TbError404 className="w-full text-8xl mb-6" />
-        <h1 className="text-4xl font-bold mb-4">Page Not Found</h1>
+        <h1 className="text-3xl font-bold mb-4">Page Not Found</h1>
         <p className="text-xl mb-6">
           Sorry, the page you are looking for doesn’t exist.
         </p>
